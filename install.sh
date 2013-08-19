8475bf7b697904c611a6ba73703905a80090cd50
Sérgio Alves <sergio2540@gmail.com>
2013-08-19 16:34:13 +0100

    add install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo npm install -g https://github.com/CrowdProcess/program-editor/archive/master.tar.gz
 sudo npm install -g crowdprocess-cli
-sudo npm install
+sudo npm install -g
 wget http://fftw.org/fftw-3.3.3.tar.gz
 tar -xvf fftw-3.3.3.tar.gz
 cd fftw-3.3.3 

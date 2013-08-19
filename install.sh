4b54954f9c4de35571ae47cd52b9a678987c6158
Sérgio Alves <sergio2540@gmail.com>
2013-08-19 18:08:55 +0100

    fix install

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 sudo npm install -g https://github.com/CrowdProcess/program-editor/archive/master.tar.gz
 sudo npm install -g crowdprocess-cli
 sudo npm install -g
+mkdir -p ./c/lib/ 
 wget http://fftw.org/fftw-3.3.3.tar.gz
 tar -xvf fftw-3.3.3.tar.gz
 cd fftw-3.3.3 
-mkdir -p ./c/lib/ 
 ./configure --disable-fortran --libdir=`readlink -f ../c/lib/`
 sudo make
 sudo make install
